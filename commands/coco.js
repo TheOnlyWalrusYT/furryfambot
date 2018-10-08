1031d1403e8140bd22d21732afdabbce19786112
TheOnlyWalrusYT <38198747+TheOnlyWalrusYT@users.noreply.github.com>
2018-10-08 14:53:45 -0400

    Create coco.js

---
+++ commands/coco.js
@@ -0,0 +1,2 @@
+const Discord = require('discord.js'); module.exports.run = async (bot, message, args) => {let embed = new Discord.RichEmbed().setImage('https://cdn.discordapp.com/attachments/489952608031735808/498676354615607311/no-mic-general_-_Discord_10_7_2018_8_43_22_PM.png'); message.channel.send(embed)}; module.exports.help = {name: "coco"}
+//Someone challenged me to write on one line
